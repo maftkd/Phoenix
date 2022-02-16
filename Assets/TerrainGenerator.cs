1f21671000e9c98838b3c615939ca5065436ef31
Michael Feldman <michael.feldman09@gmail.com>
2022-02-16 02:36:08 -0500

    started terrain gen. Boutta alter the camera

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
+	public float _baseHeight;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +23,16 @@ public class TerrainGenerator : MonoBehaviour
 		TerrainData td = t.terrainData;
 		int res = td.heightmapResolution;
 		float[,] heights = new float[res,res];
+		float r = 0.5f;
+		float maxHeight = td.size.y;
+		float b = _baseHeight/maxHeight;
 		for(int z=0;z<res;z++){
+			float zNorm = z/(float)(res-1);
 			for(int x=0;x<res;x++){
-				heights[z,x]=0;
+				float xNorm=x/(float)(res-1);
+				Vector2 diff = new Vector2(xNorm,zNorm)-Vector2.one*0.5f;
+				float dstSqr = Vector2.Dot(diff,diff);
+				heights[z,x]=dstSqr<r*r ? b: 0;
 			}
 		}
 		td.SetHeights(0,0,heights);
